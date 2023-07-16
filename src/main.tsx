7e253c6041aa38058c2d2243f1aa9674753561d4
Nanoscience202 <vincentmingli@gmail.com>
2023-07-15 20:32:52 -0400

    July 15 2023 (1)

--- src/main.tsx
+++ src/main.tsx
@@ -6,31 +6,34 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./errorPage.tsx";
 import { AboutUs, ContactUs, Home, OrderSection } from "./components";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/contact",
-        element: <ContactUs />,
-      },
-      {
-        path: "/about",
-        element: <AboutUs />,
-      },
-      {
-        path: "/order",
-        element: <OrderSection />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/contact",
+          element: <ContactUs />,
+        },
+        {
+          path: "/about",
+          element: <AboutUs />,
+        },
+        {
+          path: "/order",
+          element: <OrderSection />,
+        },
+      ],
+    },
+  ],
+  // { basename: import.meta.env.DEV ? "/" : "/react-vite-gh-pages/" }
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
